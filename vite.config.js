ee24319e72ec8b73cb74e9a63db31a3913fffc27
Fede Cuautle <fede.cuautle@gmail.com>
2023-08-22 13:03:31 -0600

    ss

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/01-tic-tac-toe/"
+  base:"/01-tic-tac-toe/",
 })
